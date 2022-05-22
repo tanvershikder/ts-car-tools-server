9fa2a6ed40c7060dee517edc695edab2916c48c7
Your Name <you@example.com>
2022-05-23 00:11:03 +0600

    user done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 4000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 //middleware
@@ -35,8 +35,24 @@ async function run() {
     try {
         await client.connect()
         const toolscollection = client.db("ts-car-tools").collection("tools");
-        const userCollection = client.db('ts-car-tools').collection('users')
+        const userCollection = client.db('ts-car-tools').collection('users');
+        const orderColelction = client.db('ts-car-tools').collection('order')
         console.log("connected");
+
+
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email
+            const requesterAccount = await userCollection.findOne({ email: requester })
+
+            if (requesterAccount.role === 'admin') {
+                next()
+            }
+            else {
+                res.status(403).send({ message: "forbidden" });
+            }
+        }
+
 
         // get all products
         app.get('/products', async (req, res) => {
@@ -46,7 +62,7 @@ async function run() {
         })
 
         //post products
-        app.post('/products',verifyJWT, async (req, res) => {
+        app.post('/products', verifyJWT, async (req, res) => {
             const product = req.body
             const result = await toolscollection.insertOne(product)
             res.send(result);
@@ -67,6 +83,96 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET)
             res.send({ result, token })
         })
+        // all users
+        app.get('/users', verifyJWT, async (req, res) => {
+
+            const result = await userCollection.find({}).toArray()
+            res.send(result)
+        })
+
+        //make user as admin
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const updateDoc = {
+                $set: { role: 'admin' },
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send({ result })
+
+        })
+
+        // get user by its role
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email })
+            const isAdmin = user.role === 'admin'
+            res.send({ admin: isAdmin })
+        })
+
+        //deleted user 
+        app.delete('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email }
+            const result = await userCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+
+        //get specefic product
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const tool = await toolscollection.findOne(query);
+            res.send(tool)
+        })
+
+        //post booking order in to database
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const result = await orderColelction.insertOne(orders);
+            res.send(result)
+        })
+
+        // Update quantity
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const quantity = req.body.updatequantity;
+            console.log(quantity);
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true };
+            const updatePro = {
+                $set: {
+                    quantity
+                }
+            }
+            const result = await toolscollection.updateOne(filter, updatePro, option);
+            res.send(result)
+        })
+
+        //get user all orders
+        app.get('/orders/:email', async (req, res) => {
+            const email = req.params.email
+            const result = await orderColelction.find({ email }).toArray()
+            res.send(result)
+        })
+
+        //get all orders
+        app.get('/orders', verifyJWT, async (req, res) => {
+
+            const result = await orderColelction.find({}).toArray()
+            res.send(result)
+        })
+
+        // delete orders 
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await orderColelction.deleteOne(filter)
+            res.send(result)
+        })
+
     }
     finally {
 
