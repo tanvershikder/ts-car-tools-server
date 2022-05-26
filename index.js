0ab44fa6442c5bb6ce5b60dbd0be3ef2eeec36de
Your Name <you@example.com>
2022-05-26 20:24:10 +0600

    'done'

--- index.js
+++ index.js
@@ -280,6 +280,8 @@ async function run() {
             const result = await paymentCollection.insertOne(payment);
             const updateOrders = await orderColelction.updateOne(filter, updateDoc)
 
+            // console.log("sending email");
+            // sendPaymentConfirmedEmail(payment.appointment)
 
             res.send(updateOrders)
         })
@@ -295,6 +297,8 @@ async function run() {
             }
             const updateOrders = await orderColelction.updateOne(filter, updateDoc)
 
+            // console.log("sending email");
+            // sendPaymentConfirmedEmail(payment.appointment)
 
             res.send(updateOrders)
         })
